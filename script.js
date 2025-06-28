77021ab9dbd75b8a13fa97079105a4714e773ca1
Nikhil Pandey <143279146+nikhilpandey03@users.noreply.github.com>
2025-06-28 21:26:08 +0530

    Second Commit

--- script.js
+++ script.js
@@ -10,9 +10,6 @@ document.addEventListener("DOMContentLoaded",function(){
  const hardLabel = document.getElementById("hard-level")
  const cardStatsContainer = document.querySelector(".stats-cards");   
 
-// ab hm check krenge ki jo username input karega vo sahi hai ki nhi hai
-// return true or false based on regular expression (regex), ye hm chatgpt se generate kara sakte hai
-// sabse pehle yser aayega aur name enter kr k search btn ko click karega
 
 function validateUsername(username){
     if(username.trim() === ""){
@@ -47,33 +44,6 @@ async function fetchUserDetails(username){
         console.log("Logging data: ", parseddata);
        
 
-
-        // agr upr wala api kaam na kare to hme khud se query generate kr k post request karni padegi
-        // const proxyUrl = 'https://cors-anywhere.herokuapp.com'
-        // const targetUrl = 'https://leetcode.com/graphql/';
-        // const myHeaders = new Headers();
-        // myHeaders.append("content-type", "application/json");
-        // const graphql =JSON.stringify({
-        //     query: "\n query userSessionProgress($username: String!) {\n allQuestionsCount {\n difficulty\n count\n }\n matchedUser(username: $username) {\n submitStats {\n acSubmissionNum \n totalSubmissionNum {\n difficulty\n count\n   submissions\n   }\n   }\n   }\n}\n  ",
-        //     variables: {"username": `${username}`}
-        // })  
-
-        // const requestOptions = {
-        //     method: "POST",
-        //     headers: myHeaders,
-        //     body: graphql,
-        //     redirect: "follow"
-        // };
-
-        // const response = await fetch(proxyUrl+targetUrl, requestOptions);
-        // if(!response.ok){
-        //     throw new Error("Unable to fetch the user details"); 
-        // }
-         
-        // const parseddata = await response.json();
-        // console.log("Logging data: ", parseddata);
-
-        // AB HAME IN DATA KO UI PR POPULATE KARNA HAI
         dispalyUserData(parseddata);
       
     }
